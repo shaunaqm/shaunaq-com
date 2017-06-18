b0d345ae8757f6668329508cbca7ec58b0b61bcc
shaunaqm <shaunaq.madan@gmail.com>
2017-06-18 14:57:11 +0530

    changed banner

--- sketch.js
+++ sketch.js
@@ -4,44 +4,49 @@ var x = [];
 var y = [];
 function setup() {
 
-  createCanvas(512, 256);
-  for (var i = 0; i<length; i++){
-  x[i] = width / 2;
-  y[i] = height / 2;
-  }
-  frameRate(30);
+	var canvas = createCanvas(512, 256);
+	canvas.parent('heading');
+
+	for (var i = 0; i<length; i++){
+		x[i] = width / 2;
+		y[i] = height / 2;
+	}
+	frameRate(30);
 
 }
 
 function draw() {
-  background(57);
+	background(255);
 
 
 // rnd = sin(13*rnd);
 strokeWeight(0);
 textSize(7);
 fill(171);
-  text(rnd, 20, 20);
-  strokeWeight(1);
+text(rnd, 0, 20);
+strokeWeight(1);
 
 
- 	
- 	for (var i = 1; i<length; i++){
-  x[i-1] = x[i];
-  y[i-1] = y[i];
-  }
-  
-  x[length-1] += rndm()*5; //random(-5,5);
-  y[length-1] += rndm()*5; //random(-5,5);
-  
-  for (var i = 1; i<length; i++){
-  stroke(i/length*200+57);
-  line(x[i-1],y[i-1],x[i],y[i]);
-  }
+
+for (var i = 1; i<length; i++){
+	x[i-1] = x[i];
+	y[i-1] = y[i];
+}
+
+x[length-1] += rndm()*5; 
+y[length-1] += rndm()*5; 
+
+x[length-1] = constrain(x[length-1], 0, width);
+y[length-1] = constrain(y[length-1], 0, height);
+
+for (var i = 1; i<length; i++){
+	stroke(255 - i/length*200);
+	line(x[i-1],y[i-1],x[i],y[i]);
+}
 
 }
 
 function rndm(){
-rnd = sin(13*rnd);
-return rnd;
+	rnd = sin(13*rnd);
+	return rnd;
 }
